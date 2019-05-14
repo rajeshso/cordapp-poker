39bf0aaa4684c6c2ef518386e954232af12498ea
Jose Pedro Novais <jpvnovais@gmail.com>
2019-05-14 23:49:37 +0100

    small refactor in Deck class

--- contracts/src/main/kotlin/com/poker/model/Deck.kt
+++ contracts/src/main/kotlin/com/poker/model/Deck.kt
@@ -1,22 +1,41 @@
 package com.poker.model
 
 import java.util.*
+import kotlin.NoSuchElementException
+import kotlin.collections.ArrayList
+import kotlin.collections.HashSet
 
-//TODO: Should this be a Singleton ? This means only one Game can be organized by the Dealer
-object Deck {
-    private var cards: MutableList<Card> = mutableListOf<Card>()
-    private var random: Random = Random()
+class Deck (_shuffledCards: List<Card>) {
+    private val cards = _shuffledCards
+    private var index = 0
+    val signature = cards.joinToString(",")
 
-    init {
-        cards = ArrayList<Card>()
-        for (suit in CardSuitEnum.values()) {
-            for (rank in CardRankEnum.values()) {
-                cards.add(Card(suit, rank))
-            }
+    fun pop(): Card {
+        return if (index < cards.size) {
+            cards.get(index++)
+        } else {
+            throw NoSuchElementException("Deck ran out of cards");
         }
     }
 
-    fun pop(): Card {
-        return cards.removeAt(random.nextInt(cards.size))
+    companion object {
+        private val allCards: HashSet<Card>
+
+        init {
+            allCards = HashSet<Card>()
+            for (suit in CardSuitEnum.values()) {
+                for (rank in CardRankEnum.values()) {
+                    allCards.add(Card(suit, rank))
+                }
+            }
+        }
+
+        fun newSheffledDeck(): Deck {
+            // Created a new Deck of Shuffled Cards
+            val cards = ArrayList<Card>(52);
+            cards.addAll(allCards)
+            Collections.shuffle(cards, Random());
+            return Deck(cards);
+        }
     }
 }
