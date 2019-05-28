ea6d1df82199eff4143ca5f007ca2cafd90ff61b
rajeshsomasundaram <rajesh.somasundaram@10xbanking.com>
2019-05-28 12:14:06 +0100

    GameUtilTest

--- workflows/src/test/kotlin/com/poker/util/GameUtilTest.kt
+++ workflows/src/test/kotlin/com/poker/util/GameUtilTest.kt
@@ -44,7 +44,7 @@ class GameUtilTest {
     }
 
     @Test
-    fun testPlayerWinDrawGameBestRanking() {
+    fun testPlayerAWinDrawGameBestRanking() {
         //Basic tests
         val game = GameUtil
         val playerA = Player(mockParty)
@@ -66,6 +66,33 @@ class GameUtilTest {
         val winnerList = game.getWinner(playerList, tableCards)
         assertThat(winnerList.size).isEqualTo(1)
         assertThat(playerA).isEqualTo(winnerList.get(0))
+        assertThat(playerA.rankingEnum).isEqualTo(ONE_PAIR)
+        assertThat(playerB.rankingEnum).isEqualTo(ONE_PAIR)
+    }
+    @Test
+    fun testPlayerBWinDrawGameHighCard() {
+        //Basic tests
+        val game = GameUtil
+        val playerA = Player(mockParty)
+        val playerB = Player(mockParty)
+        val playerList = mutableListOf<Player>(playerA, playerB)
+        val dealer =mockParty
+        val tableCards = mutableListOf<Card>()
+        val deck: Deck = Deck(dealer)
+
+        game.deal(playerList, tableCards, deck)
+        game.callFlop(playerList, tableCards, deck)
+        game.betRiver(playerList, tableCards, deck)
+        game.betTurn(playerList, tableCards, deck)
+        tableCards.clear()
+
+        tableCards.add(Card(SPADES, CARD_10))
+        playerA.myCards = listOf(Card(CLUBS, CARD_10), Card(HEARTS, ACE))
+        playerB.myCards = listOf(Card(CLUBS, CARD_2), Card(HEARTS, CARD_10))
+
+        val winnerList = game.getWinner(playerList, tableCards)
+        assertThat(winnerList.size).isEqualTo(1)
+        assertThat(playerB).isEqualTo(winnerList.get(0))
         assertThat(playerA.rankingEnum).isEqualTo(ONE_PAIR)
         assertThat(playerB.rankingEnum).isEqualTo(ONE_PAIR)
     }
